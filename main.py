20efb3fc9f80323aa712eddf2425588de066d7c9
comp22023 <comp22023@mail.ru>
2023-09-05 12:28:31 +0300

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+chislo = int(input('Введите натуральное число больше нуля: '))
+stroka = str(input('Введите строку: '))
+
+spisok=[]
+
+for i in range(1,chislo + 1):
+    spisok.append(i)
+spisok2 = []
+
+for g in spisok:
+    if (g % 2 == 0):
+        spisok2.append(stroka)
+    else:
+        spisok2.append(g)
+print(spisok2)
+#Проверка
