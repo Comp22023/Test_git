da00524a4326bf9432298f524aca8adcfc38da78
Comp22023 <comp22023@mail.ru>
2023-09-14 10:24:23 +0300

    Первый коммит, код работает не так, как планировалось

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-Ruslan = input('Введите камень, ножницы или бумага: ').lower()
-Timur = input('Введите камень, ножницы или бумага: ').lower()
+#Ruslan = input('Введите камень, ножницы или бумага: ').lower()
+#Timur = input('Введите камень, ножницы или бумага: ').lower()
+#komment
+stroka = list(input('Напишите строкой количество разыгранных орлов и решек(ОРОРРОРРРРОРРРР):'))
 
+
+counterer = 0
+
+for i in range(len(stroka)):
+    if stroka[i] == 'Р':
+        counterer += 1
+    elif stroka[i] == 'О':
+        counterer += 0
+print(counterer)
