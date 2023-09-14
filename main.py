02fc01e877604b62c31717dc01ab5d68a5931a06
Comp22023 <comp22023@mail.ru>
2023-09-14 10:57:39 +0300

    Поправка

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 #Timur = input('Введите камень, ножницы или бумага: ').lower()
 #komment
 stroka = list(input('Напишите строкой количество разыгранных орлов и решек(ОРОРРОРРРРОРРРР):'))
-
+print(stroka)
 
 counterer = 0
+spisok = []
 
 for i in range(len(stroka)):
     if stroka[i] == 'Р':
         counterer += 1
-    elif stroka[i] == 'О':
-        counterer += 0
-print(counterer)
+
+    if stroka[i] != len(stroka):
+        if stroka[i] == 'О':
+            spisok.append(counterer)
+            counterer = 0
+print(spisok)
